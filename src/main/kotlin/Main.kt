febe2bce83a4e972e2857d55594edcc518b0441b
winnieSikinyi <winniesikinyi@gmail.com>
2023-02-02 22:35:43 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+
+    var name= "Winnie_Siy"
+
+    println(name)
+
+    var age= (20)
+    println(age)
+
+    var phoneNumber= (768593188)
+    var weight= (67)
+    var kenyan:Boolean= true
+    var notKenyan:Boolean= false
+    println(kenyan)
+
+
+
+
+
+
+
+
+    }
