5d569676142454c83327761300cab8092f98db9c
winnieSikinyi <winniesikinyi@gmail.com>
2023-02-03 08:55:30 +0300

    main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,15 @@
 fun main(args: Array<String>) {
-
-    var name= "Winnie_Siy"
-
-    println(name)
-
-    var age= (20)
-    println(age)
-
-    var phoneNumber= (768593188)
+var name= ("Jeri Asy")
+    var age= (22)
     var weight= (67)
+    var phone= (768593188)
     var kenyan:Boolean= true
     var notKenyan:Boolean= false
-    println(kenyan)
+    println("kenyan")
+
+
+
+
 
 
 
